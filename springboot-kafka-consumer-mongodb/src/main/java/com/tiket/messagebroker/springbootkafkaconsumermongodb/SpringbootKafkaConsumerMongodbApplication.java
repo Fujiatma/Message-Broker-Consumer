e2ffbe6ec14f80b0264fc86d064743a67b05fda3
Fujiatma <fujiatmapratama@gmail.com>
2019-06-17 20:06:56 +0700

    first commit producer

---
+++ springboot-kafka-consumer-mongodb/src/main/java/com/tiket/messagebroker/springbootkafkaconsumermongodb/SpringbootKafkaConsumerMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tiket.messagebroker.springbootkafkaconsumermongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaConsumerMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaConsumerMongodbApplication.class, args);
+	}
+
+}
